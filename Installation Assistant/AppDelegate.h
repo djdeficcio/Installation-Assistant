f3122541ab049fbd23a879a83a7cdfca41e5d33d
DJ DeFiccio <djdeficcio@new-host-4.home>
2011-11-09 15:42:23 -0500

    Initial Commit

---
+++ Installation Assistant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Installation Assistant
+//
+//  Created by DJ DeFiccio on 11/9/11.
+//  Copyright (c) 2011 Solular, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Installation Assistant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Installation Assistant
+//
+//  Created by DJ DeFiccio on 11/9/11.
+//  Copyright (c) 2011 Solular, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
