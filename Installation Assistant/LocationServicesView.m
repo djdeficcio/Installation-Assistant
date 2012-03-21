6fea1ba1bb4bd64828384a2915094d4628ce03aa
DJ DeFiccio <djdeficcio@new-host-5.home>
2012-03-21 14:35:31 -0400

    finalized ui layout

--- Installation Assistant/LocationServicesView.m
+++ Installation Assistant/LocationServicesView.m
@@ -7,6 +7,7 @@
 //
 
 #import "LocationServicesView.h"
+#import "MenuBar.h"
 #import "Quartzcore/Quartzcore.h"
 
 @implementation LocationServicesView
@@ -20,6 +21,33 @@
         self.layer.borderWidth = 1;
         self.layer.masksToBounds = YES;
         self.layer.cornerRadius = 10;
+        
+        MenuBar *menubar = [[MenuBar alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, 50)];
+        [menubar setGradientStartColor:[UIColor colorWithRed:187.0/255.0 green:230.0/255.0 blue:252.0/255.0 alpha:1.0] AndEndColor:[UIColor colorWithRed:134.0/255.0 green:212.0/255.0 blue:253.0/255.0 alpha:1.0]];
+        menubar.title.text = @"Check In";
+        
+        [self addSubview:menubar];
+        
+        UIView *locationInfo = [[UIView alloc] initWithFrame:CGRectMake(30, 80, 230, 500)];
+        locationInfo.backgroundColor = [UIColor whiteColor];
+        locationInfo.layer.cornerRadius = 10;
+        locationInfo.layer.borderColor = [UIColor blackColor].CGColor;
+        locationInfo.layer.borderWidth = 1;
+        
+        [self addSubview:locationInfo];
+        
+        UIView *crewMembers = [[UIView alloc] initWithFrame:CGRectMake(290, 80, 230, 500)];
+        crewMembers.backgroundColor = [UIColor whiteColor];
+        crewMembers.layer.cornerRadius = 10;
+        crewMembers.layer.borderColor = [UIColor blackColor].CGColor;
+        crewMembers.layer.borderWidth = 1;
+        
+        [self addSubview:crewMembers];
+        
+        UIButton *departureButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        departureButton.frame = CGRectMake(130, 622.5, 290, 90);
+        
+        [self addSubview:departureButton];
     }
     return self;
 }
