65fac47fa5770e9c5bfd365f8ebb7340c1a42d2f
DJ DeFiccio <djdeficcio@new-host-3.home>
2012-03-28 13:11:56 -0400

    Updated Title Screen appearance

--- Installation Assistant/HomeScreenView.m
+++ Installation Assistant/HomeScreenView.m
@@ -23,7 +23,22 @@
         
         self.backgroundColor = [UIColor whiteColor];
         
-        GlosslessButton *selectProjectButton = [[GlosslessButton alloc] initWithFrame:CGRectMake(200, 200, 200, 200)];
+        UILabel *appTitle = [[UILabel alloc] initWithFrame:CGRectMake(159, 250, 450, 150)];
+        appTitle.backgroundColor = [UIColor clearColor];
+        appTitle.text = @"Field Tech";
+        appTitle.textAlignment = UITextAlignmentCenter;
+        appTitle.font = [UIFont systemFontOfSize:60];
+        
+        [self addSubview:appTitle];
+        
+        UIImageView *logo = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"solular logo.png"]];
+        logo.frame = CGRectMake(209, 25, 350, 193);
+        
+        [self addSubview:logo];
+        
+        GlosslessButton *selectProjectButton = [[GlosslessButton alloc] initWithFrame:CGRectMake(184, 500, 400, 150)];
+        selectProjectButton.textLabel.text = @"Select a Project";
+        selectProjectButton.textLabel.font = [UIFont systemFontOfSize:34];
         [selectProjectButton addTarget:parentController action:@selector(loadProjectSelection) forControlEvents:UIControlEventTouchDown];
         
         [self addSubview:selectProjectButton];
