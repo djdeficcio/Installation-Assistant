725d20779ab3ec869e804439f26c82a54caf0356
Solular <Solular@new-host-4.home>
2012-05-07 12:38:47 -0400

    Added the core data model and the sqlite database for the user login, as well as the mapped objects.

---
+++ Installation Assistant/Users.h
@@ -0,0 +1,21 @@
+//
+//  Users.h
+//  AssistantUsers
+//
+//  Created by Solular on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Users : NSManagedObject
+
+@property (nonatomic, retain) NSString * firstname;
+@property (nonatomic, retain) NSString * lastname;
+@property (nonatomic, retain) NSString * password;
+@property (nonatomic, retain) NSString * username;
+@property (nonatomic, retain) NSNumber * user_id;
+
+@end
---
+++ Installation Assistant/Users.m
@@ -0,0 +1,20 @@
+//
+//  Users.m
+//  AssistantUsers
+//
+//  Created by Solular on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Users.h"
+
+
+@implementation Users
+
+@dynamic firstname;
+@dynamic lastname;
+@dynamic password;
+@dynamic username;
+@dynamic user_id;
+
+@end
